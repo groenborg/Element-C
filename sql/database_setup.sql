c0d85871572b78b4a54b955a30ce4cc240267ccf
groenborg <groenborg27@gmail.com>
2016-05-21 17:03:51 +0200

    created fixture for project

--- sql/database_setup.sql
+++ sql/database_setup.sql
@@ -1,10 +1,9 @@
 #document for setting op table in ci
 
-CREATE TABLE user(
-    user_id INT,
-    user_name VARCHAR(100),
-    score INT,
-    uuid VARCHAR(36),
-    PRIMARY KEY (user_id)
+CREATE TABLE user(user_id INT, 
+user_name VARCHAR(100), 
+score INT, 
+uuid VARCHAR(36), 
+PRIMARY KEY (user_id)
 );
 
--- src/test/java/test/Fixture.java
+++ src/test/java/test/Fixture.java
@@ -5,18 +5,78 @@
  */
 package test;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Simon
  */
 public class Fixture {
-    
-    
-    
-    public Fixture(){
-    
-    
+
+    private Connection con;
+
+    public Fixture(Connection con) {
+        this.con = con;
     }
-    
-    
+
+    public void runScript() throws SQLException {
+        try {
+            con.setAutoCommit(false);
+            dropTable();
+            createTable();
+            insertUsers();
+            con.commit();
+        } catch (SQLException ex) {
+            con.close();
+        }
+    }
+
+    public void createTable() {
+        String createTable = "CREATE TABLE user(user_id INT, user_name VARCHAR(100), score INT, uuid VARCHAR(36), PRIMARY KEY (user_id));";
+        Statement statement;
+        try {
+            statement = con.createStatement();
+            statement.execute(createTable);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public void insertUsers() {
+
+        Statement statement = null;
+        String[] insertUser = new String[8];
+
+        insertUser[0] = "insert into user values(1,'simon',32,'')";
+        insertUser[1] = "insert into user values(1,'simon',32,'')";
+        insertUser[2] = "insert into user values(1,'simon',32,'')";
+        insertUser[3] = "insert into user values(1,'simon',32,'')";
+        insertUser[4] = "insert into user values(1,'simon',32,'')";
+        insertUser[5] = "insert into user values(1,'simon',32,'')";
+        insertUser[6] = "insert into user values(1,'simon',32,'')";
+
+        for (String dt : insertUser) {
+            try {
+                statement = con.createStatement();
+                statement.execute(dt);
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+
+    }
+
+    public void dropTable() {
+        String dropTable = "DROP TABLE user";
+        Statement statement;
+        try {
+            statement = con.createStatement();
+            statement.execute(dropTable);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
 }
