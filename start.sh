ccb28a031cec54f11bd380b4f6765e398f9c3d29
groenborg <groenborg27@gmail.com>
2016-05-23 23:27:41 +0200

    added script

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+kill $(pgrep -f "/usr/bin/java -classpath /usr/share/maven/boot/plexus-classworlds-2.x.jar -Dclassworlds")
+
 mvn exec:java -Dexec.mainClass="server.ServerExecutor" > output.txt &
